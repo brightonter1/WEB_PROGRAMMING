b7756bd3a952882920358580498a6de96b83e019
Bright <59070035@it.kmitl.ac.th>
2018-03-29 08:32:52 +0700

    no message

---
+++ Web_Programming/myscripts.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+<script>
+            var modal = document.getElementById('myModal');
+            var btn = document.getElementById("myBtn");
+            btn.onclick = function () {
+                modal.style.display = "block";
+            }
+</script>
